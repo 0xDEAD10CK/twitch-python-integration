072003c4ebf2dc5a1b7eae7a8bc61336f3cb79c1
JoshMBN <joshua.brennan.niu@gmail.com>
2024-11-28 00:27:03 +1300

    Refactor Twitch bot implementation; remove deprecated TwitchAPI.py, enhance event handling, and add support for follow events.

--- TwitchAPImain.py
+++ TwitchAPImain.py
@@ -1,13 +1,16 @@
 from twitchAPI.twitch import Twitch
 from twitchAPI.oauth import UserAuthenticator
 from twitchAPI.type import AuthScope, ChatEvent
+from twitchAPI.eventsub.websocket import EventSubWebsocket
 from twitchAPI.chat import Chat, EventData, ChatMessage, ChatSub, ChatCommand
 from obsws import *
 from dotenv import load_dotenv
 
 import os
+import subprocess
 import asyncio
 import random
+from typing import Awaitable
 
 load_dotenv() 
 
@@ -18,15 +21,11 @@ TARGET_CHANNEL = os.getenv('TARGET_CHANNEL')
 USER_SCOPE = [
     AuthScope.CHAT_READ,
     AuthScope.CHAT_EDIT,
-    #AuthScope.USER_READ_FOLLOWERS,
     AuthScope.CHANNEL_READ_SUBSCRIPTIONS,
     AuthScope.CHANNEL_READ_REDEMPTIONS,
     AuthScope.CHANNEL_MANAGE_BROADCAST,
-    #AuthScope.CHANNEL_MANAGE_POLL,
-    #AuthScope.CHANNEL_MODERATE_READ,
-    #AuthScope.USER_MANAGE_FOLLOWERS,
-    #AuthScope.USER_MANAGE_SUBSCRIPTIONS,
-    AuthScope.USER_MANAGE_BLOCKED_USERS
+    AuthScope.USER_MANAGE_BLOCKED_USERS,
+    AuthScope.MODERATOR_READ_FOLLOWERS
 ]
 
 # this will be called when the event READY is triggered, which will be on bot start
@@ -36,21 +35,22 @@ async def on_ready(ready_event: EventData):
     await ready_event.chat.join_room(TARGET_CHANNEL)
     # you can do other bot initialization things in here
 
-
 # this will be called whenever a message in a channel was send by either the bot OR another user
 async def on_message(msg: ChatMessage):
     print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
     if msg.text == "Follow":
-        alert(18, 21, msg.user.name, "Follower")
+        alert(18, 21, 24, msg.user.name, "Follower")
     if msg.text == "Sub":
-        alert(18, 21, msg.user.name, "Subscriber")
-
+        alert(18, 21, 24, msg.user.name, "Subscriber")
 
 # this will be called whenever someone subscribes to a channel
 async def on_sub(sub: ChatSub):
     print(f'New subscription in {sub.room.name}:\\n'
           f'  Type: {sub.sub_plan}\\n'
           f'  Message: {sub.sub_message}')
+
+async def on_follow(event: dict) -> Awaitable[None]:
+    alert(18, 21, 24, event.event.user_name, "Follower")
 
 # this will be called whenever the !reply command is issued
 async def test_command(cmd: ChatCommand):
@@ -62,45 +62,43 @@ async def test_command(cmd: ChatCommand):
 async def list_commands(cmd: ChatCommand):
     await cmd.reply(f"Available commands: \n!reply <message> \n!beancounter <username> \n!commands")
 
-async def paper_scissors_rock(cmd: ChatCommand):
-    if len(cmd.parameter) == 0:
-        await cmd.reply('You must choose either rock, paper, or scissors')
-    elif cmd.parameter != 'rock' or cmd.parameter != 'paper' or cmd.parameter != 'scissors':
-        await cmd.reply('You must choose either rock, paper, or scissors')
-    else:
-        robotChoice = random.choice(['rock', 'paper', 'scissors'])
-
-        if cmd.parameter == robotChoice:
-            await cmd.reply(f"{cmd.user.name} chose {cmd.parameter}, I chose {robotChoice}. It's a tie!")
-        elif cmd.parameter == 'rock' and robotChoice == 'paper':
-            await cmd.reply(f"{cmd.user.name} chose {cmd.parameter}, I chose {robotChoice}. I win!")
-        elif cmd.parameter == 'rock' and robotChoice == 'scissors':
-            await cmd.reply(f"{cmd.user.name} chose {cmd.parameter}, I chose {robotChoice}. You win!")
-        elif cmd.parameter == 'paper' and robotChoice == 'rock':
-            await cmd.reply(f"{cmd.user.name} chose {cmd.parameter}, I chose {robotChoice}. You win!")
-        elif cmd.parameter == 'paper' and robotChoice == 'scissors':
-            await cmd.reply(f"{cmd.user.name} chose {cmd.parameter}, I chose {robotChoice}. I win!")
-        elif cmd.parameter == 'scissors' and robotChoice == 'rock':
-            await cmd.reply(f"{cmd.user.name} chose {cmd.parameter}, I chose {robotChoice}. I win!")
-        elif cmd.parameter == 'scissors' and robotChoice == 'paper':
-            await cmd.reply(f"{cmd.user.name} chose {cmd.parameter}, I chose {robotChoice}. You win!")
-
-
-        
-# Example function to handle events
-async def handle_follow_event(event_data):
-    print(f"Received follow event: {event_data}")
 
 # this is where we set up the bot
 async def run():
+    global TARGET_CHANNEL
+    user = None
     # set up twitch api instance and add user authentication with some scopes
     twitch = await Twitch(APP_ID, APP_SECRET)
     auth = UserAuthenticator(twitch, USER_SCOPE)
     token, refresh_token = await auth.authenticate()
     await twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
 
+    # Fetch the user ID for the channel using async generator
+    async for user in twitch.get_users(logins=[TARGET_CHANNEL]):
+        print('1. ', user)
+        TARGET_CHANNEL_ID = user.id
+        print('2.', TARGET_CHANNEL_ID)
+        break  # Extract the first user and exit the loop
+
     # create chat instance
     chat = await Chat(twitch)
+
+    # create eventsub instance
+    eventsub = EventSubWebsocket(twitch)
+    eventsub.start()
+
+    print('3. ', user)
+    print('4. ', TARGET_CHANNEL_ID)
+
+    print(eventsub.connection_url)
+
+    # register the eventsub subscription
+    await eventsub.listen_channel_follow_v2(
+        broadcaster_user_id=str(TARGET_CHANNEL_ID),
+        moderator_user_id=str(TARGET_CHANNEL_ID),
+        callback=on_follow
+    )
+
     # register the handlers for the events you want
 
     # listen to when the bot is done starting up and ready to join channels
@@ -114,10 +112,10 @@ async def run():
     # you can directly register commands and their handlers, this will register the !reply command
     chat.register_command('reply', test_command)
     chat.register_command('commands', list_commands)
-    chat.register_command('psr', paper_scissors_rock)
 
     # we are done with our setup, lets start this bot up!
     chat.start()
+
 
 # lets run our setup
 asyncio.run(run())
--- obsws.py
+++ obsws.py
@@ -1,9 +1,15 @@
 import obsws_python as obs
 import requests
 import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OBS_PASSWORD = os.getenv('OBS_PASSWORD')
 
 # pass conn info if not in config.toml
-cl = obs.ReqClient(host='localhost', port=4455, password='password', timeout=3)
+cl = obs.ReqClient(host='localhost', port=4455, password=OBS_PASSWORD, timeout=3)
 
 # Toggle the mute state of your Mic input
 cl.toggle_input_mute('Mic/Aux')
@@ -20,12 +26,14 @@ for item in scene_items.scene_items:  # Access the scene_items attribute
     print("------")
 
 # Function to handle follow events and play animation
-def alert(sceneTextItemId, sceneImageItemId, username, type):
+def alert(sceneTextItemId, sceneImageItemId, sceneSoundItemId, username, type):
     # Show the source (e.g., FollowAnimation in OBS)
     cl.set_input_settings(name="FollowerText", settings={"text": f"New {type}: {username}"}, overlay=True)
     cl.set_scene_item_enabled(scene_name="In-Game Layout - Horizontal", item_id=sceneTextItemId, enabled=True)
     cl.set_scene_item_enabled(scene_name="In-Game Layout - Horizontal", item_id=sceneImageItemId, enabled=True)
+    cl.set_scene_item_enabled(scene_name="In-Game Layout - Horizontal", item_id=sceneSoundItemId, enabled=True)
     # Hide it after 5 seconds (or the length of your animation)
     time.sleep(5)
     cl.set_scene_item_enabled(scene_name="In-Game Layout - Horizontal", item_id=sceneTextItemId, enabled=False)
     cl.set_scene_item_enabled(scene_name="In-Game Layout - Horizontal", item_id=sceneImageItemId, enabled=False)
+    cl.set_scene_item_enabled(scene_name="In-Game Layout - Horizontal", item_id=sceneSoundItemId, enabled=False)
